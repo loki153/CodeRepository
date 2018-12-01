ce2b6314c00b68996dff6161f1eb8b319708b9ac
Loki <loki153@users.noreply.github.com>
2018-12-01 19:30:44 +0800

    Hadoop
    Hadoop demo

---
+++ RPCclient.java
@@ -0,0 +1,15 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.ipc.RPC;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class RPCclient {
+    public static void main(String[] args) throws IOException {
+        Bizable proxy = RPC.getProxy(Bizable.class,10010,new InetSocketAddress("192.168.1.107",9527),new Configuration());
+        String result = proxy.sayHi("Tomcat");
+        System.out.println(result);
+        RPC.stopProxy(proxy);
+
+    }
+}
